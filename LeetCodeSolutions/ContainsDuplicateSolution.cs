0dde1edb2e8422f37fa173cc00848bf308ea083f
NKuzmin <nkuzmin@korusconsulting.ru>
2024-01-15 20:17:12 +0300

    Добавьте файлы проекта.

---
+++ LeetCodeSolutions/ContainsDuplicateSolution.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeSolutions
+{
+    /// <summary>
+    /// https://leetcode.com/problems/contains-duplicate/
+    /// </summary>
+    class ContainsDuplicateSolution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var distinctNums = new HashSet<int>(nums);
+            return nums.Length != distinctNums.Count;
+        }
+
+        public bool ContainsDuplicateManualHashSet(int[] nums)
+        {
+            var distinctNums = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (distinctNums.Contains(nums[i]))
+                {
+                    return true;
+                }
+                distinctNums.Add(nums[i]);
+            }
+
+            return false;
+        }
+
+        public bool ContainsDuplicateSecondSolution(int[] nums)
+        {
+            Dictionary<int, int> distinctNums = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (distinctNums.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+                distinctNums.Add(nums[i], i);
+            }
+
+            return false;
+        }
+
+        public bool ContainsDuplicateNaiveSolution(int[] nums)
+        {
+            return nums.Distinct().ToArray().Length != nums.Length;
+        }
+    }
+}
---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCodeSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new ContainsDuplicateSolution().ContainsDuplicate(new int[] { 1, 2, 3, 1 });
+            Console.WriteLine(result);
+        }
+    }
+}
